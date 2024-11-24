0a722d13f8d128ac6d012ade7e2c41946e942447
iluhinoBruho <ix2.evdokimov@gmail.com>
2024-11-24 17:19:40 +0300

    allure reports

--- src/test/kotlin/com/example/demo/DemoApplicationTests.kt
+++ src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -1,13 +1,17 @@
 package com.example.demo
 
+import io.qameta.allure.Allure
+import io.qameta.allure.Step
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	fun contextLoads() {
+		Allure.step("Проверка загрузки контекста Spring")
 	}
 
 }
